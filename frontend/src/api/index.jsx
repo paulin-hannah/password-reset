cfc41917636606accd263780a96ad1d72e675bf6
paulin <vivin.rufus@gmail.com>
2024-02-10 17:56:36 +0530

    docs: api url

--- frontend/src/api/index.jsx
+++ frontend/src/api/index.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:4321',
+  baseURL: 'https://passwrd-resetb.onrender.com',
 })
 
 export const login = ({ email, password }) =>
